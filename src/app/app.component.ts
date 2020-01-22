289221f9cc229750522df32cb1743c09ffe5dfd3
David Ibl <david.ibl@xnoname.com>
2020-01-22 16:23:31 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+export interface Beruf {
+  name: string;
+  klasse: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'probezeitversicherung-rechner';
+export class AppComponent implements OnInit {
+
+  public berufe: Beruf[] = [
+    {name: 'xx', klasse: 'A'},
+    {name: 'zz', klasse: 'B'},
+    {name: 'rr', klasse: 'C'},
+    {name: 'tt', klasse: 'A'},
+    {name: 'yy', klasse: 'C'},
+  ];
+
+  public klassenTabelle = {
+    A: 0.1,
+    B: 0.2,
+    C: 0.3,
+    D: 0.4,
+  }
+
+  public gehaltSubject = new BehaviorSubject<number>(0);
+  public berufSubject = new ReplaySubject<string>(1);
+
+  public beitragSubject = combineLatest(this.gehaltSubject, this.berufSubject)
+                            .pipe(
+                              map(([gehalt, berufName]) => {
+                                if (!gehalt || !berufName) {
+                                  return 0;
+                                }
+                                const klasse = this.berufe.find(b => b.name === berufName).klasse;
+                                return gehalt * this.klassenTabelle[klasse];
+                              })
+                            );
+
+
+  public test = this.beitragSubject.pipe(tap(value => console.log(value))).subscribe();
+
+  public setBeruf(beruf: string) {
+    this.berufSubject.next(beruf);
+  }
+
+  public setGehalt(gehalt: string) {
+    this.gehaltSubject.next(parseFloat(gehalt));
+  }
+
+  public ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatInputModule, MatAutocompleteModule, MatChipsModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
